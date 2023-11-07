0dc36841fbb88865590150062358500d3352d0e4
Chaitanya Shah <shahchaitanya143@gmail.com>
2023-11-07 22:24:43 +0530

    Angular Assessment add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChoreListComponent } from './chore-list/chore-list.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { PhotosImagesComponent } from './photos-images/photos-images.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoreListComponent,
+    MovieListComponent,
+    PhotosImagesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/photos-images/photos-images.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos-images',
+  templateUrl: './photos-images.component.html',
+  styleUrls: ['./photos-images.component.css']
+})
+export class PhotosImagesComponent {
+  Jawan='../assets/Jawan.jpg';
+  RockAurRani='../assets/RockAurRani.jpg';
+  Adipurush='../assets/Adipurush.jpg';
+}
